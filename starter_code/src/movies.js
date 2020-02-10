a6b6ede2101757d0b3a1b7734d9b106c1f0bddd7
marie-pascale hardy <mariepascalehardy@mariepacalesmbp.localdomain>
2020-02-10 17:28:41 +0100

    BER WDPT Marie

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,9 +4,9 @@
 
 function orderByYear(moviesArray) {
     let copiedMoviesArray = moviesArray.slice()
-    let sortedArray = copiedMoviesArray.sort(function(a,b) {
+    let sortedArray = copiedMoviesArray.sort(function (a, b) {
         if (a.year === b.year) {
-            if (a.title > b.title){
+            if (a.title > b.title) {
                 return 1;
             } else {
                 return -1;
@@ -24,13 +24,13 @@ orderByYear(movies)
 
 function howManyMovies(moviesArray) {
     let result = 0
-    moviesArray.filter(function(movie) {
-    if (movie.genre === "Drama" && movie.director === "Steven Spielberg") {
-        result ++;
-    } else {
-        return 0
-    }
-    })    
+    moviesArray.filter(function (movie) {
+        if (movie.genre === "Drama" && movie.director === "Steven Spielberg") {
+            result++;
+        } else {
+            return 0
+        }
+    })
     return result;
 }
 
@@ -38,23 +38,23 @@ howManyMovies(movies)
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
-function orderAlphabetically(moviesArray){
+function orderAlphabetically(moviesArray) {
 
     let copiedMoviesArray = moviesArray.slice()
-    let alphabeticalArray = copiedMoviesArray.sort(function(a,b) {
-            if (a.title > b.title){
-                return 1;
-            } else {
-                return -1;
-            }
+    let alphabeticalArray = copiedMoviesArray.sort(function (a, b) {
+        if (a.title > b.title) {
+            return 1;
+        } else {
+            return -1;
+        }
     })
 
     let mappedAlphabeticalArray = alphabeticalArray.map(movie => movie.title)
 
     if (mappedAlphabeticalArray.length > 20) {
-      return mappedAlphabeticalArray.slice(0, 20)
+        return mappedAlphabeticalArray.slice(0, 20)
     } else {
-      return mappedAlphabeticalArray
+        return mappedAlphabeticalArray
     }
 }
 orderAlphabetically(movies)
@@ -65,22 +65,100 @@ orderAlphabetically(movies)
 
 
 function ratesAverage(moviesArray) {
+    if (!moviesArray) {
+        return 0
+    } else {
+        //Map array to return array with only rates
+        let mappedRatesArray = moviesArray.map(movie => movie.rate)
+        //console.log(mappedRatesArray)
 
+        //Sum all rates
+        const sum = mappedRatesArray.reduce(function (accumulator, currentValue) {
+            return accumulator + currentValue;
+        }, 0);
 
-    let mappedRatesArray = moviesArray.map(movie => movie.rate)
+        //Returns Average
+        const average = Number(Math.round(sum / mappedRatesArray.length + 'e2') + 'e-2');
 
-
-    let averageNum = mappedRatesArray.reduce(function(sum, rate) { return sum / mappedRatesArray.length },0) 
-
-    return averageNum
-
+        console.log(average);
+    }
 }
-
 ratesAverage(movies)
+
+
 
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
+
+function dramaMoviesRate(moviesArray) {
+
+    if (!moviesArray) {
+        return 0
+    } else {
+
+
+        //Filter array to return array with only drama movies
+        let dramaArray = movies.filter(movie => movie.genre.includes('Drama'))
+        //console.log(dramaArray)
+
+        //Maps array to return only rates
+        let dramaRatesArray = dramaArray.map(movie => movie.rate)
+        //console.log(dramaRatesArray)
+
+
+        //Sum all rates
+        const sum = dramaRatesArray.reduce(function (accumulator, currentValue) {
+            return accumulator + currentValue;
+        }, 0);
+
+        //Returns Average
+        const average = Number(Math.round(sum / dramaRatesArray.length + 'e2') + 'e-2');
+
+        return average
+
+    }
+
+}
+
+dramaMoviesRate(movies)
+
+
+
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+
+
+ 
+//Zero-filling function for 2 digits
+function minTwoDigits(n) {
+    return (n < 10 ? '0' : '') + n;
+  }
+  
+   function turnHoursToMinutes(moviesArray) {
+      let movieDurationMinutes = moviesArray.map(function (movie) {
+        let durationNumeric = 0
+        durationNumeric = movie.duration.match(/\d+/g).map(Number) 
+        if (durationNumeric[1] < 10) {
+           durationNumeric[1] = minTwoDigits(durationNumeric[1])
+        } else if (!durationNumeric[1]) {
+          durationNumeric[1] = '00'
+        }
+        console.log(durationNumeric)
+        //return movie
+  
+      })
+  //extract the numbers from movie.duration
+  
+  //if only 2 numbers, add a 0 in the middle
+  
+  //Then convert into minutes
+  //movie.duration * 60 / 100
+  
+  } 
+  
+   turnHoursToMinutes(movies)
+
+
+   
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
